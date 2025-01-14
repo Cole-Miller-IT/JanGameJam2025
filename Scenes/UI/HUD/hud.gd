2a6cd6f3298ebdfe1a45af3088e8c3e2f720e2f6
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2025-01-14 17:23:16 -0500

    working on hotbar focusing

--- Scenes/UI/HUD/hud.gd
+++ Scenes/UI/HUD/hud.gd
@@ -18,14 +18,38 @@ func _process(delta):
 	pass
 
 
-func _on_slot_1_pressed():
+func hotbar_pressed(hotbarKey):
 	#On press or hotkey press emit a signal that every button will use but pass in the value of the slot item to know what to do with the equipped item
 	
 	#Emit signal to the player class to swap items
-	emit_signal("playerHotbarSwap", 1) #1 for item 1
+	emit_signal("playerHotbarSwap", hotbarKey) #1 for item 1, #2 for item 2, etc..
 	
-	#Emit signal to change the hud to focus/highlight that item on the hotbar
+	#Change the hud to focus/highlight that item on the hotbar
+	changeHotbarFocus(hotbarKey)
 	
+
+func changeHotbarFocus(value):
+	print("Changing hotbar focus to button/slot " + str(value))
 	
-	
-	pass
+	#changes all of the buttons
+	$HotBar/HBoxContainer/Slot1.get_theme_stylebox("normal").bg_color = Color(0, 0.565, 0) 
+
+
+func _on_slot_1_pressed():
+	hotbar_pressed(1)
+
+
+func _on_slot_2_pressed():
+	hotbar_pressed(2)
+
+
+func _on_slot_3_pressed():
+	hotbar_pressed(3)
+
+
+func _on_slot_4_pressed():
+	hotbar_pressed(4)
+
+
+func _on_slot_5_pressed():
+	hotbar_pressed(5)
